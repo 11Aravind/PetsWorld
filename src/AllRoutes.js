8a2f27c146cd04e980f9927179c48d5b69912327
11Aravind <aravindsreenandanam11@gmail.com>
2024-01-21 03:52:26 +0530

    routerer was handled and notfound page created

--- src/AllRoutes.js
+++ src/AllRoutes.js
@@ -1,31 +1,57 @@
-//manage all router and display the router menu
 import Home from "./pages/Home";
 import Pets from "./pages/Pets";
 import Foods from "./pages/Foods";
 import Accessorys from "./pages/Accessorys";
-const routerInfo=[
-{
-    path:"/",
-    component:<Home/>,
-},
-{
-    path:"/pets",
-    component:<Pets/>
-},
-{
-    path:"/foods",
-    component:<Foods/>
-},
-{
-    path:"/accessorys",
-    component:<Accessorys/>
-}
+import {Login} from "./pages/Login";
+import {Signup} from "./pages/Signup";
+import { Routes, Route } from "react-router-dom";
+import Notfound from "./pages/Notfound";
+const routerInfo = [
+    {
+        path: "/",
+        component: <Home />,
+    },
+    {
+        path: "/pets",
+        component: <Pets />
+    },
+    {
+        path: "/foods",
+        component: <Foods />
+    },
+    {
+        path: "/accessorys",
+        component: <Accessorys />
+    },
+    {
+        path: "/login",
+        component: <Login />
+    },
+    {
+        path: "/signup",
+        component: <Signup />
+    },
+    {
+        path: "*",
+        component: <Notfound />
+    }
 ];
-const AllRouter=()=>{
-    return(
-<div>
-    <h1>All routerr component</h1>
-</div>
+const AllRouter = () => {
+    return (
+        <Routes>
+            {
+                routerInfo.map((eachRoute, id) => {
+                    return (
+                        <Route
+                            key={id}
+                            path={eachRoute.path}
+                            element={eachRoute.component}
+                        />
+                    );
+                })
+            }
+
+        </Routes>
     );
 }
 export default AllRouter;
